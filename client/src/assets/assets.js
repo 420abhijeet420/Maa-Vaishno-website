1fe6da75771d6bc298390a88af13172e232f4b31
420abhijeet420 <abhijeetkumarbomaby@gmail.com>
2026-01-08 20:36:14 +0530

    cap error fix

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg'
-import marvelLogo from './marvellogo.svg '
+import marvellogo from './marvellogo.svg '
 import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
 import screenImage from './screenImage.svg'
@@ -7,7 +7,7 @@ import profile from './profile.png'
 
 export const assets = {
     logo,
-    marvelLogo,
+    marvellogo,
     googlePlay,
     appStore,
     screenImage,
